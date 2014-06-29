5ddc93d96ee7b91c0969f52c81380441d5fd1760
nelzkie <bellchan@Macintosh.local>
2014-06-29 19:20:54 +1200

    Initia commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  C++_Primer_REVAMP
+//
+//  Created by Bellchan on 6/29/14.
+//  Copyright (c) 2014 Bellchan. All rights reserved.
+//
+
+#include <iostream>
+#include "main.h"
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
